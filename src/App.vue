92a6e8ec1365b175755ecd2db33fe9f5faf31f00
Li Swafford <liswafford@yahoo.com>
2018-09-30 22:02:04 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <app-header :maxQuotes="maxQuotes" :quoteCount="quotes.length"/>
+        <app-new-quote @quoteAdded="addQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import QuoteGrid from "./components/QuoteGrid";
+import NewQuote from "./components/NewQuote";
+import Header from "./components/Header";
+
+export default {
+  data() {
+    return {
+      quotes: ["it is Logan", "it is Li"],
+      maxQuotes: 10
+    };
+  },
+  methods: {
+    addQuote(quote) {
+      if (this.quotes.length >= this.maxQuotes) {
+        return alert("Delete some events before adding more");
+      }
+      this.quotes.push(quote);
+    },
+
+    deleteQuote(i) {
+      this.quotes.splice(i, 1);
+    }
+  },
+  components: {
+    appQuoteGrid: QuoteGrid,
+    appNewQuote: NewQuote,
+    appHeader: Header
+  }
+};
+</script>
+
+<style>
+</style>
